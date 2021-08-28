76dac5b0ac456e745f06f38c4fdf8e41c84db3fd
Rodolfo P Graciotti <rodolfo.graciotti@gmail.com>
2021-08-28 15:40:37 -0300

    Add files via upload

---
+++ Data/shuffle_random_state_recall.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import StandardScaler
+from tpot.builtins import StackingEstimator
+from tpot.export_utils import set_param_recursive
+
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1)
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'], random_state=78329)
+
+# Average CV score on the training set was: 0.7028571428571427
+exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=RandomForestClassifier(bootstrap=True, criterion="gini", max_features=0.7500000000000001, min_samples_leaf=2, min_samples_split=14, n_estimators=100)),
+    StandardScaler(),
+    BernoulliNB(alpha=0.01, fit_prior=False)
+)
+# Fix random state for all the steps in exported pipeline
+set_param_recursive(exported_pipeline.steps, 'random_state', 78329)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
